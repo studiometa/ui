293cc4175258986ba018ebac411e8e05a7bc0acc
Titouan Mathis <titouan@studiometa.fr>
2021-10-06 17:59:48 +0200

    Cleanup Jest config

--- packages/tests/__setup__/mockRequestIdleCallback.js
+++
@@ -1,9 +0,0 @@
-const callbacks = [];
-
-globalThis.requestIdleCallback = function (callback) {
-  callbacks.push(callback);
-};
-
-export function mockRequestIdleCallback() {
-  callbacks.forEach((callback) => callback());
-}
