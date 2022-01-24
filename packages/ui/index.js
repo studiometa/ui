efe4303a40a211b3e57adbee8bc9944f163aed0f
Titouan Mathis <titouan@studiometa.fr>
2022-01-24 16:34:42 +0100

    Lint files

--- packages/ui/index.js
+++ packages/ui/index.js
@@ -1,4 +1,4 @@
 export * from './primitives/index.js';
 export * from './atoms/index.js';
 export * from './molecules/index.js';
-export * from './organisms/index.js';
+// export * from './organisms/index.js';
