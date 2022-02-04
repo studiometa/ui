e33f5e7eb5bf676d302fb561c0b8db45f69f42a9
Titouan Mathis <titouan@studiometa.fr>
2022-02-04 15:24:43 +0100

    Improve usage of the `RenderTwig` component

--- packages/docs/.symfony/src/Controller/ApiController.php
+++ packages/docs/.symfony/src/Controller/ApiController.php
@@ -10,20 +10,26 @@ use Symfony\Component\Routing\Annotation\Route;
 final class ApiController extends AbstractController
 {
     /**
-     * @Route("/api", name="api", methods={"GET"})
+     * @Route("/api", name="api", methods={"GET","POST"})
      */
     public function index(Request $request): Response
     {
         $query = $request->query;
         $path = $query->get('path');
-        $tpl = $query->get('tpl');
+        $tpl = $request->getContent();
 
         if (empty($path) && empty($tpl)) {
             die('Nothing to display.');
         }
 
         if (!empty($tpl)) {
-            return $this->render("@private/render-from-tpl.html.twig", $query->all());
+            return $this->render(
+                '@private/render-from-tpl.html.twig',
+                array_merge(
+                    $query->all(),
+                    ['tpl' => $tpl],
+                )
+            );
         }
 
         return $this->render($query->get('path'), $query->all());
