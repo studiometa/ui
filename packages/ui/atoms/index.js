196f1737736649478e0ae2bf693bd6e72c5e7e16
Titouan Mathis <titouan@studiometa.fr>
2022-02-28 22:00:18 +0100

    Add doc page for the `LazyInclude` atom

--- packages/ui/atoms/index.js
+++ packages/ui/atoms/index.js
@@ -1,2 +1,3 @@
-export { default as Figure } from './Figure/Figure.js';
 export { default as Cursor } from './Cursor/Cursor.js';
+export { default as Figure } from './Figure/Figure.js';
+export { default as LazyInclude } from './LazyInclude/LazyInclude.js';
