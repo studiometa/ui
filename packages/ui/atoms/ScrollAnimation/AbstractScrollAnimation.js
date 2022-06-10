084ab52a7302d25874aea380250907c2507a362f
Titouan Mathis <titouan@studiometa.fr>
2022-06-10 14:20:02 +0200

    Use the `animate` function to animate on scroll

--- packages/ui/atoms/ScrollAnimation/AbstractScrollAnimation.js
+++ packages/ui/atoms/ScrollAnimation/AbstractScrollAnimation.js
@@ -1,40 +1,10 @@
 import { Base, withFreezedOptions } from '@studiometa/js-toolkit';
-import { matrix, lerp, map, clamp } from '@studiometa/js-toolkit/utils';
+import { map, clamp, animate } from '@studiometa/js-toolkit/utils';
 
 /**
  * @typedef {typeof AbstractScrollAnimation} AbstractScrollAnimationConstructor
- * @typedef {{
- *   x?: number;
- *   y?: number;
- *   z?: number;
- *   scale?: number;
- *   scaleX?: number;
- *   scaleY?: number;
- *   rotate?: number;
- *   skewX?: number;
- *   skewY?: number;
- *   opacity?: number;
- * }} AnimationOptions
+ * @typedef {import('@studiometa/js-toolkit/utils/css/animate.js').Keyframe} Keyframe
  */
-
-/**
- * Get animation configuration defaults.
- * @returns {AnimationOptions}
- */
-function getDefaults() {
-  return {
-    x: 0,
-    y: 0,
-    z: 0,
-    scale: 1,
-    scaleX: 1,
-    scaleY: 1,
-    rotate: 0,
-    skewX: 0,
-    skewY: 0,
-    opacity: 1,
-  };
-}
 
 /**
  * @typedef {AbstractScrollAnimation & {
@@ -42,8 +12,9 @@ function getDefaults() {
  *     playRange: string,
  *     dampFactor: number,
  *     dampPrecision: number,
- *     from: AnimationOptions,
- *     to: AnimationOptions,
+ *     from: Keyframe,
+ *     to: Keyframe,
+ *     keyframes: Keyframe[],
  *   },
  * }} ScrollAnimationChildInterface
  */
@@ -82,13 +53,18 @@ export default class AbstractScrollAnimation extends withFreezedOptions(Base) {
       },
       from: {
         type: Object,
-        default: getDefaults,
-        merge: true,
+        default: () => ({}),
       },
       to: {
         type: Object,
-        default: getDefaults,
-        merge: true,
+        default: () => ({}),
+      },
+      keyframes: {
+        type: Array,
+      },
+      easing: {
+        type: Array,
+        default: () => [0, 0, 1, 1],
       },
     },
   };
@@ -103,44 +79,29 @@ export default class AbstractScrollAnimation extends withFreezedOptions(Base) {
   }
 
   /**
-   * Flags for style detection.
+   * @type {ReturnType<animate>}
    */
-  get has() {
-    const has = {
-      x: false,
-      y: false,
-      z: false,
-      scale: false,
-      scaleX: false,
-      scaleY: false,
-      rotate: false,
-      skewX: false,
-      skewY: false,
-      opacity: false,
-      transform: false,
-    };
+  animation;
 
-    Object.keys(this.$options.from).forEach((key) => {
-      has[key] = this.$options.from[key] !== this.$options.to[key];
-    });
+  /**
+   * @this {ScrollAnimationChildInterface}
+   * @returns {void}
+   */
+  mounted() {
+    let { keyframes } = this.$options;
+    const { from, to } = this.$options;
 
-    has.transform = Object.keys(has)
-      .filter((key) => key !== 'opacity' && key !== 'transform')
-      .some((key) => has[key]);
+    if (keyframes.length <= 0 && from && to) {
+      keyframes = [from, to];
+    }
 
-    Object.defineProperty(this, 'has', { value: has });
-
-    return has;
+    this.animation = animate(this.target, keyframes, { easing: this.$options.easing });
   }
 
   /**
    * @todo do not add unnecessary styles
    */
   scrolledInView(props) {
-    if (!this.has.opacity && !this.has.transform) {
-      return;
-    }
-
     const progress = map(
       clamp(props.dampedProgress.y, this.$options.playRange[0], this.$options.playRange[1]),
       this.$options.playRange[0],
@@ -159,52 +120,6 @@ export default class AbstractScrollAnimation extends withFreezedOptions(Base) {
    * @returns {void}
    */
   render(progress) {
-    if (this.has.opacity) {
-      this.target.style.opacity = String(
-        map(progress, 0, 1, this.$options.from.opacity, this.$options.to.opacity)
-      );
-    }
-
-    if (this.has.transform) {
-      let transform = matrix({
-        translateX: this.has.x
-          ? lerp(this.$options.from.x, this.$options.to.x, progress)
-          : undefined,
-        translateY: this.has.y
-          ? lerp(this.$options.from.y, this.$options.to.y, progress)
-          : undefined,
-        // eslint-disable-next-line no-nested-ternary
-        scaleX: this.has.scale
-          ? lerp(this.$options.from.scale, this.$options.to.scale, progress)
-          : this.has.scaleX
-          ? lerp(this.$options.from.scaleX, this.$options.to.scaleX, progress)
-          : undefined,
-        // eslint-disable-next-line no-nested-ternary
-        scaleY: this.has.scale
-          ? lerp(this.$options.from.scale, this.$options.to.scale, progress)
-          : this.has.scaleY
-          ? lerp(this.$options.from.scaleY, this.$options.to.scaleY, progress)
-          : undefined,
-        skewX: this.has.skewX
-          ? lerp(this.$options.from.skewX, this.$options.to.skewX, progress)
-          : undefined,
-        skewY: this.has.skewY
-          ? lerp(this.$options.from.skewY, this.$options.to.skewY, progress)
-          : undefined,
-      });
-
-      if (this.has.rotate) {
-        transform += ` rotate(${lerp(
-          this.$options.from.rotate,
-          this.$options.to.rotate,
-          progress
-        )}deg)`;
-      }
-
-      transform += `translateZ(${
-        this.has.z ? lerp(this.$options.from.z, this.$options.to.z, progress) : 0
-      })`;
-      this.target.style.transform = transform;
-    }
+    this.animation.progress(progress);
   }
 }
