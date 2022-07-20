35144d85b674c62f24ea5b3628fe4e753a12815c
Titouan Mathis <titouan@studiometa.fr>
2022-07-20 09:28:40 +0200

    Improve export-size analysis by including an export of everything

---
+++ scripts/update-exports-for-export-size.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const fs = require('fs');
+
+const index = path.resolve(__dirname, '../packages/ui/index.js');
+
+const content = `import * as PRIMITIVES from './primitives/index.js';
+import * as ATOMS from './atoms/index.js';
+import * as MOLECULES from './molecules/index.js';
+import * as ORGANISMS from './organisms/index.js';
+
+export * from './primitives/index.js';
+export * from './atoms/index.js';
+export * from './molecules/index.js';
+export * from './organisms/index.js';
+
+export {
+  PRIMITIVES,
+  ATOMS,
+  MOLECULES,
+  ORGANISMS,
+};
+
+export const ALL = {
+  PRIMITIVES,
+  ATOMS,
+  MOLECULES,
+  ORGANISMS,
+};
+`;
+
+fs.writeFileSync(index, content, { encoding: 'UTF-8' });
