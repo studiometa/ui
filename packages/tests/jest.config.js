63d754d1163936c891e5c599f2be26928b9287e7
Titouan Mathis <titouan@studiometa.fr>
2022-09-16 10:25:36 +0200

    Fix tests

--- packages/tests/jest.config.js
+++ packages/tests/jest.config.js
@@ -1,5 +1,5 @@
 /**
- * @link https://jestjs.io/docs/en/configuration.html
+ * @see https://jestjs.io/docs/en/configuration.html
  * @type {import('@jest/types').Config.GlobalConfig}
  */
 export default {
@@ -8,10 +8,17 @@ export default {
     pretendToBeVisual: true,
   },
   collectCoverage: false,
-  collectCoverageFrom: ['<rootDir>/ui/**/*.js'],
+  collectCoverageFrom: ['<rootDir>/ui/**/*.js', '<rootDir>/ui/**/*.ts'],
   rootDir: '..',
   transform: {
+    '^.+\\.ts$': ['esbuild-jest', { format: 'esm', sourcemap: true }],
     '^.+\\.html?$': 'html-loader-jest',
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  resolver: 'ts-jest-resolver',
+  moduleNameMapper: {
+    '^@studiometa/ui$': '<rootDir>/ui',
+    '^@studiometa/ui/(.*)': '<rootDir>/ui$1',
   },
   globals: {
     __DEV__: true,
