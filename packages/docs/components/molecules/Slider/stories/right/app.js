0d5f767b1a959b3647701571f92876648fe2003b
Titouan Mathis <titouan@studiometa.fr>
2022-10-10 18:03:07 +0200

    Slider: add more exmples to the docs

---
+++ packages/docs/components/molecules/Slider/stories/right/app.js
@@ -0,0 +1,15 @@
+import { Base, createApp } from '@studiometa/js-toolkit';
+import { Figure } from '@studiometa/ui';
+import Slider from './Slider.js';
+
+class App extends Base {
+  static config = {
+    name: 'App',
+    components: {
+      Figure,
+      Slider,
+    },
+  };
+}
+
+export default createApp(App, document.body);
