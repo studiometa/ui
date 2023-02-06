4e41f10d091682295f8b06ebb7c493852544ffa7
Titouan Mathis <titouan@studiometa.fr>
2023-02-06 13:54:33 +0100

    Format file

--- packages/playground/tailwind.config.cjs
+++ packages/playground/tailwind.config.cjs
@@ -1,25 +1,25 @@
 module.exports = {
-	darkMode: 'class',
-	content: [
-		'./src/js/**/*.ts',
-		'./src/js/**/*.vue',
-		'./src/templates/**/*.twig',
-		'./src/templates/**/*.yml',
-		'./node_modules/@studiometa/ui/**/*.twig',
-		'./node_modules/@studiometa/ui/**/*.js',
-		'tailwind.safelist.txt',
-	],
-	theme: {
-		extend: {
-			keyframes: {
-				loader: {
-					'0%': { transform: 'rotate(0)' },
-					'100%': { transform: 'rotate(720deg)' },
-				},
-			},
-			animation: {
-				loader: 'loader 1s ease-in-out infinite',
-			}
-		},
-	},
+  darkMode: 'class',
+  content: [
+    './src/js/**/*.ts',
+    './src/js/**/*.vue',
+    './src/templates/**/*.twig',
+    './src/templates/**/*.yml',
+    './node_modules/@studiometa/ui/**/*.twig',
+    './node_modules/@studiometa/ui/**/*.js',
+    'tailwind.safelist.txt',
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        loader: {
+          '0%': { transform: 'rotate(0)' },
+          '100%': { transform: 'rotate(720deg)' },
+        },
+      },
+      animation: {
+        loader: 'loader 1s ease-in-out infinite',
+      }
+    },
+  },
 };
