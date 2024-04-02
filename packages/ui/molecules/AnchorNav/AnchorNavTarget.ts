14df69947d6ac5f2b3ee90bec880c4c0e49058cc
Clara Martin <clara@studiometa.fr>
2024-04-02 14:51:37 +0200

    Add the withIntersectionObserver on the componenet AnchorNavTarget

--- packages/ui/molecules/AnchorNav/AnchorNavTarget.ts
+++ packages/ui/molecules/AnchorNav/AnchorNavTarget.ts
@@ -1,4 +1,4 @@
-import { Base } from '@studiometa/js-toolkit';
+import { Base, withIntersectionObserver } from '@studiometa/js-toolkit';
 import type { BaseProps, BaseConfig } from '@studiometa/js-toolkit';
 
 export interface AnchorNavTargetProps extends BaseProps {
@@ -10,65 +10,26 @@ export interface AnchorNavTargetProps extends BaseProps {
 /**
  * Manage a sticky table section.
  */
-export class AnchorNavTarget extends Base<AnchorNavTargetProps> {
+export class AnchorNavTarget extends withIntersectionObserver(Base, {
+  threshold: 0.5,
+  rootMargin: '0% 0px -40% 0px',
+})<AnchorNavTargetProps> {
   /**
    * Config.
    */
   static config: BaseConfig = {
     name: 'AnchorNavTarget',
-    refs: ['item[]'],
-    options: {
-      threshold: { type: String, default: '0.5' },
-      rootmargin: { type: String, default: '0% 0px -40% 0px' },
-    },
     emits: ['is-intersected'],
   };
 
-  mounted() {
-    console.log('mounted');
-
-    const observer = new IntersectionObserver(this.checkIntersection.bind(this), {
-      threshold: this.$options.threshold,
-      rootMargin: this.$options.rootmargin,
-    });
-    this.$refs.item.forEach((target) => {
-      observer.observe(target);
-    });
-  }
-
-  checkIntersection(entries) {
+  intersected(entries) {
     console.log(entries);
-
     if (entries[0].isIntersecting) {
+      console.log(this.$options);
+
       let { id } = entries[0].target;
       id = `#${id}`;
-      console.log(id);
-
       this.$emit('is-intersected', id);
     }
   }
 }
-
-/**
- * Manage a sticky table section.
- */
-// export class AnchorNavTarget extends withIntersectionObserver(Base, {
-//   threshold: 0.5,
-//   rootMargin: '0% 0px -40% 0px',
-// })<AnchorNavTargetProps> {
-//   /**
-//    * Config.
-//    */
-//   static config: BaseConfig = {
-//     name: 'AnchorNavTarget',
-//   };
-
-  // intersected(entries) {
-  //   if (entries[0].isIntersecting) {
-  //     console.log(this.$options);
-
-  //     let { id } = entries[0].target;
-  //     id = `#${id}`;
-  //     this.$emit('is-intersected', id);
-  //   }
-// }
