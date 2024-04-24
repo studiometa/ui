0ba588e984180e9aa5648b1d049c16d44d470c77
Antoine Quatrelivre <antoine@studiometa.fr>
2024-04-24 18:48:10 +0200

    Update Navbar demo with RAF

--- packages/docs/demos/Navbar/stories/app.js
+++ packages/docs/demos/Navbar/stories/app.js
@@ -1,130 +1,68 @@
 import { Base, createApp } from 'https://cdn.skypack.dev/@studiometa/js-toolkit';
-import { tween, lerp, easeOutQuad } from 'https://cdn.skypack.dev/@studiometa/js-toolkit/utils';
-
+import { clamp, damp, transform } from 'https://cdn.skypack.dev/@studiometa/js-toolkit/utils';
 class Navbar extends Base {
   static config = {
     name: 'Navbar',
-  };
-
-  translateY = 0;
-
-  lastDirection = 'UP';
-
-  animation = null;
-
-  stopAnimation() {
-    if(this.animation) {
-      this.animation.pause();
-      this.animation = null;
-    }
   }
 
-  hide(options) {
-    this.stopAnimation();
+  y = 0;
+  dampedY = 0;
+  dampFactor = 0.7;
+  dampFactorAuto = 0.7;
+  dampFactorMethod = 0.1;
+  lastDirection = null;
 
-    const { height } = this.$el.getBoundingClientRect();
-
-    if (this.translateY === -height) {
-      return;
-    }
-
-    const from = this.translateY;
-    const to = -height;
-
-    this.animation = tween(
-      (progress) => {
-        const value = lerp(from, to, progress);
-        this.$el.style.transform = `translateY(${this.translateY}px)`;
-        this.translateY = value;
-      },
-      {
-        duration: 0.3,
-        easing: easeOutQuad,
-        ...options,
-      },
-    );
-
-    this.animation.start();
+  get targetY() {
+    return this.$el.offsetHeight * -1;
   }
 
-  show(options) {
-    this.stopAnimation();
-
-    if (this.translateY === 0) {
-      return;
-    }
-
-    const from = this.translateY;
-    const to = 0;
-
-    this.animation = tween(
-      (progress) => {
-        const value = lerp(from, to, progress);
-        this.$el.style.transform = `translateY(${this.translateY}px)`;
-        this.translateY = value;
-      },
-      {
-        duration: 0.3,
-        easing: easeOutQuad,
-        ...options,
-      },
-    );
-
-    this.animation.start();
-  }
-
-  toggle(options = {}, condition) {
-    if ((condition ?? (this.translateY > -(this.$el.getBoundingClientRect().height / 3)))) {
-      return this.show(options);
-    } else {
-      return this.hide(options);
-    }
-}
-
-  onFocusin() {
-    this.show();
-  }
-
-  onMouseenter() {
-    this.show();
-  }
-
-  scrolled({ direction, y, delta, changed, progress }) {
-    const { height } = this.$el.getBoundingClientRect();
-
-    this.stopAnimation();
-
+  scrolled({ direction, delta, progress }) {
     if (direction.y === 'NONE') {
-      if ((this.translateY !== height || this.translateY !== 0) && (y > height || this.lastDirection === 'UP') && progress.y < 1) {
-        this.toggle({ delay: 1 }, this.lastDirection === 'UP');
+      if (this.lastDirection === 'UP') {
+        this.show();
+        return;
       }
-      return;
-    }
-
-    if (!changed.y) {
+      this.hide();
       return;
     }
 
     this.lastDirection = direction.y;
 
     if (progress.y < 0) {
-      this.translateY = 0;
+      this.y = 0;
       return;
     }
 
     if (progress.y >= 1) {
-      this.translateY = -height;
+      this.y = this.targetY();
       return;
     }
 
-    const value = Math.min(Math.max(this.translateY - delta.y, -height), 0);
+    this.dampFactor = this.dampFactorAuto;
+    this.y = clamp(this.y - delta.y, 0, this.targetY);
+    this.$services.enable('raf');
+  }
 
-    if (value === this.translateY) {
-      return;
+  ticked() {
+    this.dampedY = damp(this.y, this.dampedY, this.dampFactor);
+    transform(this.$el, {
+      y: this.dampedY,
+    });
+    if (this.dampedY === this.y) {
+      this.$services.disable('raf');
     }
+  }
 
-    this.translateY = value;
-    this.$el.style.transform = `translateY(${this.translateY}px)`;
+  show() {
+    this.dampFactor = this.dampFactorMethod;
+    this.y = 0;
+    this.$services.enable('raf');
+  }
+
+  hide() {
+    this.dampFactor = this.dampFactorMethod;
+    this.y = this.targetY;
+    this.$services.enable('raf');
   }
 }
 
