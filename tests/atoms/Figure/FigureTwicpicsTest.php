c59d5e7a9087298200b1ac7b5193b5047a8e9b38
Titouan Mathis <titouan@studiometa.fr>
2024-07-03 13:40:08 +0200

    Add tests

---
+++ tests/atoms/Figure/FigureTwicpicsTest.php
@@ -0,0 +1,55 @@
+<?php
+
+test('it does not render a lazy image', function () {
+    assertTwigMatchesSnapshot("
+        {% include '@ui/atoms/Figure/FigureTwicpics.twig' with {
+            src: '/uploads/image.jpg',
+            width: 400,
+            height: 400,
+            lazy: false,
+            twic_domain: 'test.twic.pics',
+            twic_path: 'path'
+        } %}
+    ");
+});
+
+
+test('it renders a lazy image by default', function () {
+    assertTwigMatchesSnapshot("
+        {% include '@ui/atoms/Figure/FigureTwicpics.twig' with {
+            src: '/uploads/image.jpg',
+            width: 400,
+            height: 400,
+            twic_domain: 'test.twic.pics',
+            twic_path: 'path'
+        } %}
+    ");
+});
+
+test('it can render TwicPics placeholders', function () {
+    assertTwigMatchesSnapshot("
+        {% include '@ui/atoms/Figure/FigureTwicpics.twig' with {
+            src: '/uploads/image.jpg',
+            width: 400,
+            height: 400,
+            twic_domain: 'test.twic.pics',
+            twic_path: 'path',
+            twic_placeholder: 'preview'
+        } %}
+    ");
+});
+
+test('it can render advanced TwicPics placeholders', function () {
+    assertTwigMatchesSnapshot("
+        {% include '@ui/atoms/Figure/FigureTwicpics.twig' with {
+            src: '/uploads/image.jpg',
+            width: 400,
+            height: 400,
+            twic_domain: 'test.twic.pics',
+            twic_path: 'path',
+            twic_placeholder: {
+                quality_max: 1,
+            },
+        } %}
+    ");
+});
