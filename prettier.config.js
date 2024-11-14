dcbd9e53c868569a24a6ac9a671f4271d99c2a7c
Titouan Mathis <titouan@studiometa.fr>
2024-11-14 10:13:34 +0100

    Update Prettier

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+import prettier from '@studiometa/prettier-config';
+
+export default {
+  ...prettier,
+  overrides: [
+    {
+      files: ['packages/docs/components/**/*.twig', 'packages/docs/components/**/*.js'],
+      options: {
+        printWidth: 80,
+        twigPrintWidth: 80,
+      },
+    },
+  ],
+};
