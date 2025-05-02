1602643cabff41dca0beab508d9eb5e98a6030a8
Titouan Mathis <titouan@studiometa.fr>
2025-05-02 16:19:49 +0200

    Fix an initialization error

--- packages/ui/Frame/index.ts
+++ packages/ui/Frame/index.ts
@@ -1,6 +1,6 @@
-export * from './AbstractFrameTrigger.js';
 export * from './Frame.js';
 export * from './FrameAnchor.js';
 export * from './FrameForm.js';
 export * from './FrameLoader.js';
 export * from './FrameTarget.js';
+export * from './AbstractFrameTrigger.js';
