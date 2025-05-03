817e3977efc864407f5066d7611d53b47cbead54
Titouan Mathis <titouan@studiometa.fr>
2025-05-04 00:18:51 +0200

    Add support for twoslash in the docs

--- packages/docs/.vitepress/theme/index.js
+++ packages/docs/.vitepress/theme/index.js
@@ -1,10 +1,13 @@
 import DefaultTheme from 'vitepress/theme';
+import TwoslashFloatingVue from '@shikijs/vitepress-twoslash/client';
 import Badge from './components/Badge.vue';
 import Badges from './components/Badges.vue';
 import PreviewPlayground from './components/PreviewPlayground.vue';
 import IframePreviewPlayground from './components/IframePreviewPlayground.vue';
 import TableOfContent from './components/TableOfContent.vue';
 import './custom.css';
+
+import '@shikijs/vitepress-twoslash/style.css';
 
 export default {
   extends: DefaultTheme,
@@ -15,5 +18,6 @@ export default {
     app.component('IframePreviewPlayground', IframePreviewPlayground);
     app.component('TableOfContent', TableOfContent);
     app.component('Toc', TableOfContent);
+    app.use(TwoslashFloatingVue);
   },
 };
