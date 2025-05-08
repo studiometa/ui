b36e9db826bbcacb8342bb5247db5d1a32c42f89
Titouan Mathis <titouan@studiometa.fr>
2025-05-08 11:39:18 +0200

    ⚠️ Refactor Draggable to add support for bounds

--- packages/ui/Draggable/Draggable.ts
+++ packages/ui/Draggable/Draggable.ts
@@ -1,16 +1,36 @@
-import { Base, withDrag } from '@studiometa/js-toolkit';
+import { Base, withDrag, DragService } from '@studiometa/js-toolkit';
 import type { BaseProps, BaseConfig, DragServiceProps } from '@studiometa/js-toolkit';
-import { domScheduler, transform } from '@studiometa/js-toolkit/utils';
+import { domScheduler, transform, damp, clamp } from '@studiometa/js-toolkit/utils';
+
+export interface DraggableProps extends BaseProps {
+  $refs: {
+    target: HTMLElement;
+  };
+  $options: {
+    fitBounds: boolean;
+    sensitivity: number;
+    dropSensitivity: number;
+  };
+}
 
 /**
  * Draggable class.
  */
-export class Draggable<T extends BaseProps = BaseProps> extends withDrag(Base)<T> {
+export class Draggable<T extends BaseProps = BaseProps> extends withDrag(Base, {
+  // @ts-expect-error draggable is instance of Draggable.
+  target: (draggable) => draggable.target,
+})<T & DraggableProps> {
   /**
    * Config.
    */
   static config: BaseConfig = {
     name: 'DraggableElement',
+    refs: ['target'],
+    options: {
+      fitBounds: Boolean,
+      sensitivity: { type: Number, default: 0.5 },
+      dropSensitivity: { type: Number, default: 0.1 },
+    },
   };
 
   /**
@@ -34,22 +54,93 @@ export class Draggable<T extends BaseProps = BaseProps> extends withDrag(Base)<T
   originY = 0;
 
   /**
+   * Smoothed horizontal transformation.
+   */
+  dampedX = 0;
+
+  /**
+   * Smoothed vertical transformation.
+   */
+  dampedY = 0;
+
+  /**
+   * Smooth factor.
+   */
+  dampFactor = 0.5;
+
+  /**
+   * The draggable element, defaults to `this.$refs.target`.
+   */
+  get target(): HTMLElement {
+    return this.$refs.target;
+  }
+
+  /**
+   * The bouding element, defaults to `this.$el`.
+   */
+  get parent(): HTMLElement {
+    return this.$el;
+  }
+
+  /**
+   * The bounds values.
+   */
+  get bounds() {
+    const { target, parent } = this;
+    const { offsetTop, offsetHeight, offsetLeft, offsetWidth, offsetParent } = target;
+    const yMin = offsetParent === parent ? offsetTop : offsetTop - parent.offsetTop;
+    const xMin = offsetParent === parent ? offsetLeft : offsetLeft - parent.offsetLeft;
+    const yMax = yMin + offsetHeight - parent.offsetHeight;
+    const xMax = xMin + offsetWidth - parent.offsetWidth;
+
+    return {
+      yMin,
+      yMax,
+      xMin,
+      xMax,
+    };
+  }
+
+  /**
    * Drag service hook.
    */
   dragged(props: DragServiceProps) {
-    if (props.mode === 'start') {
+    if (props.mode === DragService.MODES.START) {
       this.originX = this.x;
       this.originY = this.y;
-      return;
+      this.dampFactor = this.$options.sensitivity;
+      this.render();
+    } else if (
+      props.mode === DragService.MODES.DRAG ||
+      (props.mode === DragService.MODES.INERTIA && !this.$options.fitBounds)
+    ) {
+      this.x = this.originX + props.x - props.origin.x;
+      this.y = this.originY + props.y - props.origin.y;
+      this.render();
+    } else if (props.mode === DragService.MODES.DROP && this.$options.fitBounds) {
+      const { bounds } = this;
+      this.x = clamp(this.x, bounds.xMin, bounds.xMax);
+      this.y = clamp(this.y, bounds.yMin, bounds.yMax);
+      this.dampFactor = this.$options.dropSensitivity;
+      this.$services.enable('ticked');
     }
+  }
 
-    this.x = this.originX + props.x - props.origin.x;
-    this.y = this.originY + props.y - props.origin.y;
+  ticked() {
+    this.render();
+    if (this.dampedX === this.x && this.dampedY === this.y) {
+      this.$services.disable('ticked');
+    }
+  }
+
+  render() {
+    this.dampedX = damp(this.x, this.dampedX, this.dampFactor);
+    this.dampedY = damp(this.y, this.dampedY, this.dampFactor);
 
     domScheduler.write(() => {
-      transform(this.$el, {
-        x: this.x,
-        y: this.y,
+      transform(this.target, {
+        x: this.dampedX,
+        y: this.dampedY,
       });
     });
   }
