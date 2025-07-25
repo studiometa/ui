f3195a9a35d6d7bf86e7bad5d686947a57a6562c
Titouan Mathis <titouan@studiometa.fr>
2025-07-25 19:52:41 +0200

    Use a lngLat options instead of 2 separate options

--- packages/ui/MapboxMap/MapboxPopup.ts
+++ packages/ui/MapboxMap/MapboxPopup.ts
@@ -1,12 +1,14 @@
-import { Base, BaseConfig, BaseProps, getClosestParent } from '@studiometa/js-toolkit';
+import { type BaseConfig, type BaseProps } from '@studiometa/js-toolkit';
 import { Popup, PopupOptions } from 'mapbox-gl';
-import { MapboxMap } from './MapboxMap.js';
+import {
+  AbstractMapboxMapChild,
+  type AbstractMapboxMapChildProps,
+} from './AbstractMapboxMapChild.js';
 
-export interface MapboxPopupProps extends BaseProps {
+export interface MapboxPopupProps extends AbstractMapboxMapChildProps {
   $el: HTMLTemplateElement;
   $options: {
-    lat: number;
-    lng: number;
+    lngLat: [number, number];
     /**
      * Popup options.
      * @see https://docs.mapbox.com/mapbox-gl-js/api/markers#popup
@@ -15,12 +17,23 @@ export interface MapboxPopupProps extends BaseProps {
   };
 }
 
-export class MapboxPopup<T extends BaseProps = BaseProps> extends Base<T & MapboxPopupProps> {
+/**
+ * Display a popup on a MapboxMap map.
+ * @see https://ui.studiometa.dev/-/components/MapboxMap
+ */
+export class MapboxPopup<T extends BaseProps = BaseProps> extends AbstractMapboxMapChild<
+  T & MapboxPopupProps
+> {
+  /**
+   * Config.
+   */
   static config: BaseConfig = {
     name: 'MapboxPopup',
     options: {
-      lng: Number,
-      lat: Number,
+      lngLat: {
+        type: Array,
+        default: () => [0, 0],
+      },
       popupOptions: Object,
     },
   };
@@ -35,10 +48,6 @@ export class MapboxPopup<T extends BaseProps = BaseProps> extends Base<T & Mapbo
     return this.__popup;
   }
 
-  get map() {
-    return getClosestParent(this, MapboxMap)?.map;
-  }
-
   get popupOptions() {
     if (this.$options.popupOptions) {
       return this.$options.popupOptions;
@@ -47,12 +56,8 @@ export class MapboxPopup<T extends BaseProps = BaseProps> extends Base<T & Mapbo
     return {};
   }
 
-  get lngLat(): [number, number] {
-    return [this.$options.lng, this.$options.lat];
-  }
-
   mounted() {
-    this.popup.setLngLat(this.lngLat).addTo(this.map);
+    this.popup.setLngLat(this.$options.lngLat).addTo(this.map);
 
     if (this.$el.content.childNodes.length === 1) {
       this.popup.setDOMContent(this.$el.content.firstChild);
