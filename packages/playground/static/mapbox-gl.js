92cfc433e5da565477312a2c1f5c63ef2af1fa2f
Titouan Mathis <titouan@studiometa.fr>
2025-07-25 19:53:11 +0200

    Update docs

--- packages/playground/static/mapbox-gl.js
+++ packages/playground/static/mapbox-gl.js
@@ -1,4 +1,4 @@
 import mapboxgl from 'mapbox-gl';
-export { Map, Marker, Popup } from 'mapbox-gl';
+export { Map, Marker, Popup, NavigationControl, GeolocateControl } from 'mapbox-gl';
 
 export default mapboxgl;
