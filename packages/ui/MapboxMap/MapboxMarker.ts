56644023e93d7192bfc0b14d7d0d055dd7dabd50
Titouan Mathis <titouan@studiometa.fr>
2025-07-30 17:49:56 +0200

    Add support for attaching a popup to a marker

--- packages/ui/MapboxMap/MapboxMarker.ts
+++ packages/ui/MapboxMap/MapboxMarker.ts
@@ -1,11 +1,15 @@
 import { type BaseProps, type BaseConfig } from '@studiometa/js-toolkit';
 import { Marker } from 'mapbox-gl';
 import { AbstractMapboxMapChild, AbstractMapboxMapChildProps } from './AbstractMapboxMapChild.js';
+import { MapboxPopup } from './MapboxPopup.js';
 
 export interface MapboxMarkerProps extends AbstractMapboxMapChildProps {
   $options: {
     lngLat: [number, number];
     markerOptions: any;
+  };
+  $children: {
+    MapboxPopup: MapboxPopup[];
   };
 }
 
@@ -26,6 +30,9 @@ export class MapboxMarker<T extends BaseProps = BaseProps> extends AbstractMapbo
       // Marker options. (https://docs.mapbox.com/mapbox-gl-js/api/markers#marker)
       markerOptions: Object,
     },
+    components: {
+      MapboxPopup,
+    },
   };
 
   __marker: Marker;
@@ -38,6 +45,10 @@ export class MapboxMarker<T extends BaseProps = BaseProps> extends AbstractMapbo
     return this.__marker;
   }
 
+  get popup() {
+    return this.$children.MapboxPopup[0];
+  }
+
   get markerOptions() {
     if (this.$options.markerOptions) {
       return this.$options.markerOptions;
@@ -48,6 +59,9 @@ export class MapboxMarker<T extends BaseProps = BaseProps> extends AbstractMapbo
 
   mounted() {
     this.marker.setLngLat(this.$options.lngLat).addTo(this.map);
+    if (this.popup) {
+      this.marker.setPopup(this.popup.popup);
+    }
   }
 
   destroyed() {
