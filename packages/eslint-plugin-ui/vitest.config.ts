e8fdef970989b3a06fa414bf70b09eb995d89595
Titouan Mathis <titouan@studiometa.fr>
2026-05-11 14:32:50 +0200

    Configure CI for @studiometa/eslint-plugin-ui tests and publish

--- packages/eslint-plugin-ui/vitest.config.ts
+++ packages/eslint-plugin-ui/vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['src/**/*.test.ts'],
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/*.test.ts', 'src/utils/rule-tester.ts'],
+    },
   },
 });
